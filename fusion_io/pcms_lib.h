2c33f124a4d65b33fc218614a054b92314d2818c
Angelyr <scardking@gmail.com>
2025-01-11 12:37:34 -0800

    1d 2d 3d serialize functions

--- fusion_io/pcms_lib.h
+++ fusion_io/pcms_lib.h
@@ -8,17 +8,6 @@
 #include <pcms/field.h>
 #include <pcms/types.h>
 
-namespace {
-  template <typename T>
-  struct IndirectionLevel {
-      static const int value = 0;
-  };
-  template <typename T>
-  struct IndirectionLevel<T*> {
-      static const int value = IndirectionLevel<T>::value + 1;
-  };
-}
-
 namespace fusion_io {
   class Library {
     public:
@@ -57,16 +46,13 @@ namespace fusion_io {
     const int& dim_;
   };
 
-  template <typename fieldtype>
+  template <typename fieldType>
   class FieldAdapter {
     public:
-      using value_type = std::remove_pointer_t<std::remove_pointer_t<fieldtype>>;
-      fieldtype* field;
-      int size[3];
-      int totalSize;
-      std::vector<pcms::GO> gids_;
+      using value_type = std::remove_pointer_t<std::remove_pointer_t<std::remove_pointer_t<fieldType>>>;
+      using memory_space = pcms::HostMemorySpace;
 
-      FieldAdapter(fieldtype* fieldIn, int sizeX=1, int sizeY=1, int sizeZ=1) :
+      FieldAdapter(fieldType fieldIn, int sizeX=1, int sizeY=1, int sizeZ=1) :
         field(fieldIn), 
         size{sizeX, sizeY, sizeZ} {
         int totalSize = sizeX*sizeY*sizeZ;
@@ -88,10 +74,14 @@ namespace fusion_io {
         return reverse_partition;
       }
 
-      template < typename = typename std::enable_if< IndirectionLevel<fieldtype>::value==1 > >
-      int Serialize(pcms::ScalarArrayView<fieldtype, pcms::HostMemorySpace> buffer,
-                    pcms::ScalarArrayView<const pcms::LO, pcms::HostMemorySpace> permutation) const
+      //Serialize 1D Pointers
+      template <typename U = fieldType>
+      typename std::enable_if<std::is_pointer<U>::value 
+                          && !std::is_pointer<typename std::remove_pointer<U>::type>::value, int>::type
+      Serialize(pcms::ScalarArrayView<value_type, memory_space> buffer,
+                    pcms::ScalarArrayView<const pcms::LO, memory_space> permutation) const
       {
+        REDEV_ALWAYS_ASSERT(buffer.size() == permutation.size());
         if (buffer.size() > 0) {
           for (int i = 0; i < totalSize; i++) {
             buffer[i] = field[permutation[i]];
@@ -100,21 +90,92 @@ namespace fusion_io {
         return totalSize;
       }
 
-      template < typename = typename std::enable_if< IndirectionLevel<fieldtype>::value==1 > >
-      void Deserialize(pcms::ScalarArrayView<const fieldtype, pcms::HostMemorySpace> buffer,
-                       pcms::ScalarArrayView<const pcms::LO, pcms::HostMemorySpace> permutation) const
+      //Deserialize 1D Pointers
+      template <typename U = fieldType>
+      typename std::enable_if<std::is_pointer<U>::value 
+                          && !std::is_pointer<typename std::remove_pointer<U>::type>::value, void>::type
+      Deserialize(pcms::ScalarArrayView<const value_type, memory_space> buffer,
+                       pcms::ScalarArrayView<const pcms::LO, memory_space> permutation) const
       {
         REDEV_ALWAYS_ASSERT(buffer.size() == permutation.size());
         for (int i = 0; i < buffer.size(); ++i) {
           field[permutation[i]] = buffer[i];
         }
       }
-      
-      template <typename T1, typename T2>
-      int Serialize(T1, T2) const noexcept { return 0;}
 
-      template <typename T1, typename T2>
-      void Deserialize(T1, T2) const noexcept {}
+      //Serialize 2D Pointers
+      template <typename U = fieldType>
+      typename std::enable_if<std::is_pointer<typename std::remove_pointer<U>::type>::value 
+                          && !std::is_pointer<typename std::remove_pointer<typename std::remove_pointer<U>::type>::type>::value, int>::type
+      Serialize(pcms::ScalarArrayView<value_type, memory_space> buffer,
+                    pcms::ScalarArrayView<const pcms::LO, memory_space> permutation) const
+      {
+        REDEV_ALWAYS_ASSERT(buffer.size() == permutation.size());
+        if (buffer.size() > 0) {
+          for (int i = 0; i < totalSize; i++) {
+            int x = permutation[i] % size[0];
+            int y = permutation[i] / size[0];
+            buffer[i] = field[x][y];
+          }
+        }
+        return totalSize;
+      }
+
+      //Deserialize 2D Pointers
+      template <typename U = fieldType>
+      typename std::enable_if<std::is_pointer<typename std::remove_pointer<U>::type>::value 
+                          && !std::is_pointer<typename std::remove_pointer<typename std::remove_pointer<U>::type>::type>::value, void>::type
+      Deserialize(pcms::ScalarArrayView<const value_type, memory_space> buffer,
+                       pcms::ScalarArrayView<const pcms::LO, memory_space> permutation) const
+      {
+        REDEV_ALWAYS_ASSERT(buffer.size() == permutation.size());
+        for (int i = 0; i < buffer.size(); ++i) {
+          int x = permutation[i] % size[0];
+          int y = permutation[i] / size[0];
+          field[x][y] = buffer[i];
+        }
+      }
+
+      //Serialize 3D Pointers
+      template <typename U = fieldType>
+      typename std::enable_if<std::is_pointer<typename std::remove_pointer<typename std::remove_pointer<U>::type>::type>::value 
+                          && !std::is_pointer<typename std::remove_pointer<typename std::remove_pointer<typename std::remove_pointer<U>::type>::type>::type>::value, int>::type
+      Serialize(pcms::ScalarArrayView<value_type, memory_space> buffer,
+                    pcms::ScalarArrayView<const pcms::LO, memory_space> permutation) const
+      {
+        REDEV_ALWAYS_ASSERT(buffer.size() == permutation.size());
+        if (buffer.size() > 0) {
+          for (int i = 0; i < totalSize; i++) {
+            int z = permutation[i] / (size[0] * size[1]);
+            int y = (permutation[i] / size[0]) % size[1];
+            int x = permutation[i] % size[0];
+            buffer[i] = field[x][y][z];
+          }
+        }
+        return totalSize;
+      }
+
+      //Deserialize 3D Pointers
+      template <typename U = fieldType>
+      typename std::enable_if<std::is_pointer<typename std::remove_pointer<typename std::remove_pointer<U>::type>::type>::value 
+                          && !std::is_pointer<typename std::remove_pointer<typename std::remove_pointer<typename std::remove_pointer<U>::type>::type>::type>::value, void>::type
+      Deserialize(pcms::ScalarArrayView<const value_type, memory_space> buffer,
+                       pcms::ScalarArrayView<const pcms::LO, memory_space> permutation) const
+      {
+        REDEV_ALWAYS_ASSERT(buffer.size() == permutation.size());
+        for (int i = 0; i < buffer.size(); ++i) {
+          int z = permutation[i] / (size[0] * size[1]);
+          int y = (permutation[i] / size[0]) % size[1];
+          int x = permutation[i] % size[0];
+          field[x][y][z] = buffer[i];
+        }
+      }
+
+    private:
+      fieldType field;
+      int size[3];
+      int totalSize;
+      std::vector<pcms::GO> gids_;
   };
 }
 
